747ba2225eca2332ec3db503930012879394f604
SuvajitMajhi <suvajit.majhi19.work@gmail.com>
2024-08-22 13:25:43 +0530

    done

--- problem1.sql
+++ problem1.sql
@@ -1,1 +1,6 @@
-
+CREATE Table song(
+    Title varchar(255),
+    Artist varchar(255)
+);
+INSERT into song (Title, Artist) VALUES ('Let it Be', 'Beatles') ;
+INSERT into song (Title, Artist) VALUES ('Imagine', 'Beatles');
